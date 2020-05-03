85af53322b4fe723409cf60b4ef8742428c4c97b
chm10 <christian.maekawa@gmail.com>
2020-05-03 12:02:28 -0300

    #18 - Added final version to simulated_annealing

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from searches import best
 from searches import dfs
 from searches import astar
 from searches import bfs
+from searches import anne
 from tools.desenha_mapa import desenha_mapa
 
 def main():
@@ -27,10 +28,11 @@ def main():
 
     fp.close()
     #path = dfs.depth_first_search(mapa, inicio, fim)
-    path = astar.aestrela(mapa, inicio, fim)
+    #path = astar.aestrela(mapa, inicio, fim)
     #path = bfs.breadth_first_search(mapa,inicio,fim)
     #path = best.best_first_search(mapa, inicio, fim)
-
+    path = anne.simulated_annealing(mapa,inicio,fim)
+    
     if (path != None):
         print()
         print('Nos expandidos')
