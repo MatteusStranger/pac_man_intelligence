faab95ae0d6614f9ff904b1d2dff2e482db4858d
matteus.vargas <vargas.simao@gmail.com>
2020-05-03 23:53:50 -0300

    Adicionado interações com o usuário durante a execução. Opções de execução, com seus respectivos tempos. Remoção de variáveis não utilizadas

--- main.py
+++ main.py
@@ -6,6 +6,60 @@ from searches import anne
 from tools.desenha_mapa import desenha_mapa
 from randomfill.generate import map_template
 import io
+from timeit import Timer
+
+
+def desenha_caminho(path, mapa, largura, altura, inicio, fim):
+    if (path != None):
+        print()
+        print('Nos expandidos')
+        print()
+        print(path)
+        print()
+        desenha_mapa(mapa, largura, altura, espaco=1, path=path, inicio=inicio, objetivo=fim)
+        print()
+        print('Contagem: {0}'.format(len(path)))
+        print()
+    else:
+        print('Não há caminho')
+
+
+def executa(n_times, mapa, inicio, fim, largura, altura, i):
+    if (i == 0):
+        print()
+        print('DFS')
+        t = Timer(lambda: dfs.depth_first_search(mapa, inicio, fim))
+        print(f"Time spend to run  DFS algorithm {t.timeit(number=n_times) / n_times}")
+        path = dfs.depth_first_search(mapa, inicio, fim)
+        desenha_caminho(path, mapa, largura, altura, inicio, fim)
+    if (i == 1):
+        print()
+        print('AStar')
+        t2 = Timer(lambda: astar.aestrela(mapa, inicio, fim))
+        print(f"Time spend to run A* algorithm {t2.timeit(number=n_times) / n_times}")
+        path = astar.aestrela(mapa, inicio, fim)
+        desenha_caminho(path, mapa, largura, altura, inicio, fim)
+    if (i == 2):
+        print()
+        print('BFS')
+        t3 = Timer(lambda: bfs.breadth_first_search(mapa, inicio, fim))
+        print(f"Time spend to run BFS algorithm {t3.timeit(number=n_times) / n_times}")
+        path = bfs.breadth_first_search(mapa, inicio, fim)
+        desenha_caminho(path, mapa, largura, altura, inicio, fim)
+    if (i == 3):
+        print()
+        print('Best First')
+        t4 = Timer(lambda: dfs.depth_first_search(mapa, inicio, fim))
+        print(f"Time spend to run Best first search algorithm {t4.timeit(number=n_times) / n_times}")
+        path = best.best_first_search(mapa, inicio, fim)
+        desenha_caminho(path, mapa, largura, altura, inicio, fim)
+    if (i == 4):
+        print()
+        print('Simulated Annealing')
+        t5 = Timer(lambda: anne.simulated_annealing(mapa, inicio, fim))
+        print(f"Time spend to run anneling algorithm {t5.timeit(number=n_times) / n_times}")
+        path = anne.simulated_annealing(mapa, inicio, fim)
+        desenha_caminho(path, mapa, largura, altura, inicio, fim)
 
 
 def main():
@@ -16,39 +70,73 @@ def main():
     largura = 0
     altura = 0
     static_map = False
-    if static_map:
-        fp = open('./maze/maze.in', 'r')
-    else:
-        fp = io.StringIO(map_template())
-    while len(chars) > 0:
-        chars = [str(i) for i in fp.readline().strip()]
-        largura = len(chars) if largura == 0 else largura
-        for x in range(len(chars)):
-            mapa[(x, altura)] = chars[x]
-            if (chars[x] == '@'):
-                inicio = (x, altura)
-            elif (chars[x] == '$'):
-                fim = (x, altura)
-        if (len(chars) > 0):
-            altura += 1
 
-    fp.close()
-    # path = dfs.depth_first_search(mapa, inicio, fim)
-    # path = astar.aestrela(mapa, inicio, fim)
-    # path = bfs.breadth_first_search(mapa,inicio,fim)
-    # path = best.best_first_search(mapa, inicio, fim)
-    path = anne.simulated_annealing(mapa, inicio, fim)
+    print('Pacman, o nosso herói, deve usar todo o seu raciocínio e sagacidade para chegar ao seu objetivo.')
+    print('Ele foi colocado em um labirinto com um objetivo final a ser alcançado.')
+    print('Para isso, ele deve comer pílulas pelo caminho, vencer fantasmas, desviar de paredes e superar obstáculos.')
+    print(
+        'Existe o desafio casual, com um mapa estático definido ou o hard, onde um mapa novo é gerado a cada execução.')
+    print('Ele tem 5 algoritmos diferentes que podem ser usados para alcançar o objetivo:')
+    print('BFS, DFS, AStar, Best e o Simulated Annealing')
+    print('It`s time')
+    print()
+    print('Escolha o tipo de mapa:')
+    print('1. Mapa estático')
+    print('2. Mapa aleatório')
+    opcoes = int(input())
 
-    if (path != None):
-        print()
-        print('Nos expandidos')
-        print()
-        desenha_mapa(mapa, largura, altura, espaco=1, path=path, inicio=inicio, objetivo=fim)
-        print()
-        print('Contagem: {0}'.format(len(path)))
-        print()
+    if (opcoes == 1):
+        static_map = True
+    elif (opcoes == 2):
+        static_map = False
     else:
-        print('Não há caminho')
+        print('Opção inválida')
+
+    if (opcoes in (1, 2)):
+        if static_map:
+            fp = open('./maze/maze.in', 'r')
+        else:
+            fp = io.StringIO(map_template())
+        while len(chars) > 0:
+            chars = [str(i) for i in fp.readline().strip()]
+            largura = len(chars) if largura == 0 else largura
+            for x in range(len(chars)):
+                mapa[(x, altura)] = chars[x]
+                if (chars[x] == '@'):
+                    inicio = (x, altura)
+                elif (chars[x] == '$'):
+                    fim = (x, altura)
+            if (len(chars) > 0):
+                altura += 1
+
+        fp.close()
+
+        print()
+        print('Os algoritmos executados trazem o tempo de execução')
+
+        # opcoes = int(input())
+
+        print()
+        print('1. Executa todos os algoritmos')
+        print('2. Escolha o algoritmo')
+        opcoes = int(input())
+        path = ''
+        if (opcoes == 1):
+            for i in range(5):
+                executa(500, mapa, inicio, fim, largura, altura, i)
+        elif (opcoes == 2):
+            print('0. DFS')
+            print('1. AStar')
+            print('2. BFS')
+            print('3. Best')
+            print('4. Simulated Annealing')
+            i = int(input())
+            if (i in range(0, 5)):
+                executa(500, mapa, inicio, fim, largura, altura, i)
+            else:
+                print('Opção inválida')
+        else:
+            print('Opção Inválida')
 
 
 if __name__ == "__main__": main()
--- searches/anne.py
+++ searches/anne.py
@@ -1,59 +1,58 @@
 import numpy as np
 from tools import estrutura_dados as No
 
-def simulated_annealing(mapa, inicio, fim, T0=1000, M=20000, N=15, alpha=0.999, k=1):
-	inicio_no = No.No(inicio, None)
-	objetivo_no = No.No(fim, None)
-	path = []
-	no_atual = inicio_no
-	(x, y) = no_atual.posicao
-	(goal_x,goal_y) = objetivo_no.posicao
-	path.append(no_atual.posicao)
-	while T0 > 2:
-		for j in range(N):
-			while T0 > 2:
-				for j in range(N):
-					rand_amp = np.random.rand()
-					choose_x_y = np.random.rand()
-					step_x = 0
-					step_y = 0
-					if choose_x_y >= 0.5:
-						step_x = k * (1 if rand_amp < 0.5 else -1)
-					else:
-						step_y = -k * (1 if rand_amp < 0.5 else -1)
 
-					x_temporary = x + step_x
-					y_temporary = y + step_y
+def simulated_annealing(mapa, inicio, fim, T0=1000, N=15, alpha=0.999, k=1):
+    inicio_no = No.No(inicio, None)
+    objetivo_no = No.No(fim, None)
+    path = []
+    no_atual = inicio_no
+    (x, y) = no_atual.posicao
+    (goal_x, goal_y) = objetivo_no.posicao
+    path.append(no_atual.posicao)
+    while T0 > 2:
+        for j in range(N):
+            rand_amp = np.random.rand()
+            choose_x_y = np.random.rand()
+            step_x = 0
+            step_y = 0
+            if choose_x_y >= 0.5:
+                step_x = k * (1 if rand_amp < 0.5 else -1)
+            else:
+                step_y = -k * (1 if rand_amp < 0.5 else -1)
 
-					obj_mov_possible = (x_temporary - goal_x) ** 2 + (y_temporary - goal_y) ** 2
+            x_temporary = x + step_x
+            y_temporary = y + step_y
 
-					obj_val_current = (x_temporary - x) ** 2 + (y_temporary - y) ** 2
+            obj_mov_possible = (x_temporary - goal_x) ** 2 + (y_temporary - goal_y) ** 2
 
-					rand_factor = np.random.rand()
+            obj_val_current = (x_temporary - x) ** 2 + (y_temporary - y) ** 2
 
-					probality_eq = 1 / (np.exp((obj_mov_possible - obj_val_current) / T0))
+            rand_factor = np.random.rand()
 
-					content = mapa.get((x_temporary, y_temporary))
-					if ((content == '#') or (content == '&') or (content == ' ')):
-						continue
+            probality_eq = 1 / (np.exp((obj_mov_possible - obj_val_current) / T0))
 
-					if (obj_mov_possible <= obj_val_current) | (rand_factor <= probality_eq):
-						no_atual = No.No((x,y),(x_temporary,y_temporary))
-						x = x_temporary
-						y = y_temporary
-						path.append(no_atual.posicao)
-					else:
-						if (obj_mov_possible <= obj_val_current) | (rand_factor <= probality_eq):
-							no_atual = No.No((x,y),(x_temporary,y_temporary))
-							x = x_temporary
-							y = y_temporary
-							path.append(no_atual.posicao)
-						else:
-							x = x
-							y = y
+            content = mapa.get((x_temporary, y_temporary))
+            if ((content == '#') or (content == '&') or (content == ' ')):
+                continue
 
-					if no_atual == objetivo_no:
-						return path[::-1]
+            if (obj_mov_possible <= obj_val_current) | (rand_factor <= probality_eq):
+                no_atual = No.No((x, y), (x_temporary, y_temporary))
+                x = x_temporary
+                y = y_temporary
+                path.append(no_atual.posicao)
+            else:
+                if (obj_mov_possible <= obj_val_current) | (rand_factor <= probality_eq):
+                    no_atual = No.No((x, y), (x_temporary, y_temporary))
+                    x = x_temporary
+                    y = y_temporary
+                    path.append(no_atual.posicao)
+                else:
+                    x = x
+                    y = y
 
-					T0 = alpha * T0
-	return None
+            if no_atual == objetivo_no:
+                return path[::-1]
+
+            T0 = alpha * T0
+    return None
