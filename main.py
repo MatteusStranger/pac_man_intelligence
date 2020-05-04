053518a23b5676ae0bde112672eb274fca1d5aca
senfernandes <senfernandes@gmail.com>
2020-05-04 19:30:48 -0300

    Adiciona report em .txt dos algoritmos executados

--- main.py
+++ main.py
@@ -8,6 +8,16 @@ from randomfill.generate import map_template
 import io
 from timeit import Timer
 
+def clear_report():
+    f = open("report.txt", "w")
+    f.write('')
+    f.close()
+
+def write_text(text):
+    f = open("report.txt", "a")
+    print(text)
+    f.write(text + '\n')
+    f.close()
 
 def desenha_caminho(path, mapa, largura, altura, inicio, fim):
     if (path != None):
@@ -18,46 +28,45 @@ def desenha_caminho(path, mapa, largura, altura, inicio, fim):
         print()
         desenha_mapa(mapa, largura, altura, espaco=1, path=path, inicio=inicio, objetivo=fim)
         print()
-        print('Contagem: {0}'.format(len(path)))
+        write_text('Contagem: {0}'.format(len(path)))
         print()
     else:
         print('Não há caminho')
 
-
 def executa(n_times, mapa, inicio, fim, largura, altura, i):
     if (i == 0):
         print()
-        print('DFS')
+        write_text('DFS')
         t = Timer(lambda: dfs.depth_first_search(mapa, inicio, fim))
-        print(f"Time spend to run  DFS algorithm {t.timeit(number=n_times) / n_times}")
+        write_text(f"Time spent to run  DFS algorithm {t.timeit(number=n_times) / n_times}")
         path = dfs.depth_first_search(mapa, inicio, fim)
         desenha_caminho(path, mapa, largura, altura, inicio, fim)
     if (i == 1):
         print()
-        print('AStar')
+        write_text('AStar')
         t2 = Timer(lambda: astar.aestrela(mapa, inicio, fim))
-        print(f"Time spend to run A* algorithm {t2.timeit(number=n_times) / n_times}")
+        write_text(f"Time spent to run A* algorithm {t2.timeit(number=n_times) / n_times}")
         path = astar.aestrela(mapa, inicio, fim)
         desenha_caminho(path, mapa, largura, altura, inicio, fim)
     if (i == 2):
         print()
-        print('BFS')
+        write_text('BFS')
         t3 = Timer(lambda: bfs.breadth_first_search(mapa, inicio, fim))
-        print(f"Time spend to run BFS algorithm {t3.timeit(number=n_times) / n_times}")
+        write_text(f"Time spent to run BFS algorithm {t3.timeit(number=n_times) / n_times}")
         path = bfs.breadth_first_search(mapa, inicio, fim)
         desenha_caminho(path, mapa, largura, altura, inicio, fim)
     if (i == 3):
         print()
-        print('Best First')
+        write_text('Best First')
         t4 = Timer(lambda: dfs.depth_first_search(mapa, inicio, fim))
-        print(f"Time spend to run Best first search algorithm {t4.timeit(number=n_times) / n_times}")
+        write_text(f"Time spent to run Best first search algorithm {t4.timeit(number=n_times) / n_times}")
         path = best.best_first_search(mapa, inicio, fim)
         desenha_caminho(path, mapa, largura, altura, inicio, fim)
     if (i == 4):
         print()
-        print('Simulated Annealing')
+        write_text('Simulated Annealing')
         t5 = Timer(lambda: anne.simulated_annealing(mapa, inicio, fim))
-        print(f"Time spend to run anneling algorithm {t5.timeit(number=n_times) / n_times}")
+        write_text(f"Time spent to run anneling algorithm {t5.timeit(number=n_times) / n_times}")
         path = anne.simulated_annealing(mapa, inicio, fim)
         desenha_caminho(path, mapa, largura, altura, inicio, fim)
 
@@ -74,16 +83,17 @@ def main():
     print('Pacman, o nosso herói, deve usar todo o seu raciocínio e sagacidade para chegar ao seu objetivo.')
     print('Ele foi colocado em um labirinto com um objetivo final a ser alcançado.')
     print('Para isso, ele deve comer pílulas pelo caminho, vencer fantasmas, desviar de paredes e superar obstáculos.')
-    print(
-        'Existe o desafio casual, com um mapa estático definido ou o hard, onde um mapa novo é gerado a cada execução.')
+    print('Existe o desafio casual, com um mapa estático definido ou o hard, onde um mapa novo é gerado a cada execução.')
     print('Ele tem 5 algoritmos diferentes que podem ser usados para alcançar o objetivo:')
-    print('BFS, DFS, AStar, Best e o Simulated Annealing')
+    print('BFS, DFS, AStar, Best First Search e o Simulated Annealing')
     print('It`s time')
     print()
     print('Escolha o tipo de mapa:')
     print('1. Mapa estático')
     print('2. Mapa aleatório')
     opcoes = int(input())
+
+    clear_report()
 
     if (opcoes == 1):
         static_map = True
@@ -121,6 +131,7 @@ def main():
         print('2. Escolha o algoritmo')
         opcoes = int(input())
         path = ''
+
         if (opcoes == 1):
             for i in range(5):
                 executa(500, mapa, inicio, fim, largura, altura, i)
@@ -128,7 +139,7 @@ def main():
             print('0. DFS')
             print('1. AStar')
             print('2. BFS')
-            print('3. Best')
+            print('3. Best First Search')
             print('4. Simulated Annealing')
             i = int(input())
             if (i in range(0, 5)):
