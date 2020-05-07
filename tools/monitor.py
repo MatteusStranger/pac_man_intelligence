50404515eebe03c6cbbc846d7113ac9188147308
matteus.vargas <vargas.simao@gmail.com>
2020-05-07 18:55:42 -0300

    Melhorias no monitor

--- tools/monitor.py
+++ tools/monitor.py
@@ -4,20 +4,31 @@ from tools.user_interaction import write_text
 
 
 def monitor():
-    print()
-    p = psutil.Process(os.getpid())
-    print("Uso de CPU ", end='')
-    cpu = p.cpu_percent(interval=15)
-    print((f"{cpu}%"))
-    write_text(f"Uso de CPU {cpu}%")
-    print()
+    #### CPU #####
 
     print()
-    mem = (p.memory_percent() * 100)
-    print(f"Uso de memória {mem}%")
-    write_text(f"Uso de memória {mem}%")
+    p = psutil.Process(os.getpid())
+    print("Uso de CPU: ", end='')
+    cpu = psutil.cpu_percent(interval=0.0)
+    print((f"{cpu:0.2f}%"))
+    write_text(f"Uso de CPU: {cpu:0.2f}%")
     print()
+
+    ##### Memory ####
+
+    mem = (p.memory_percent())
+    print(f"Uso de memoria: {mem}%")
+    write_text(f"Uso de memoria: {mem}%")
+    print()
+
+    print(f"Uso de swap: {p.memory_full_info().swap}%")
+    write_text(f"Uso de swap: {p.memory_full_info().swap}%")
+    print()
+
+    #### Processos ####
+
     pid = len(psutil.pids())
-    print(f"PIDS (processos rodando no sistema) {pid}")
-    write_text(f"Processos em execução {pid}")
+    print(f"PIDS (processos rodando no sistema): {pid}")
+    write_text(f"PIDS (processos rodando no sistema): {pid}")
     print()
+
